7b35a73f3beda90a74605997e335500de3e4fec4
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-06-08 02:14:13 +0600

    user roll added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-// // jwt
-// const jwt = require('jsonwebtoken');
+// jwt
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -24,6 +24,26 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+// jwt verify start 
+const verifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+// jwt verify end
+
+
 async function run() {
   try {
 
@@ -32,23 +52,88 @@ async function run() {
     const usersCollection = client.db('dbAssignment12').collection('users');
     // server link end 
 
+    // jwt localhost start
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '2h'
+      });
+      res.send({ token });
+    })
+    // jwt localhost end
+
+    // Warning: use verifyJWT before using verifyAdmin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next();
+    }
 
     // user information post dataBD start 
     app.post('/users', async (req, res) => {
       const user = req.body;
 
-      // // google sign up part start
-      // const query = { email: user.email }
-      // const existingUser = await usersCollection.findOne(query);
-      // if (existingUser) {
-      //   return res.send({ message: 'user already exists' })
-      // }
-      // // google sign up part start
+      // google sign up part start
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+      // google sign up part start
 
       const result = await usersCollection.insertOne(user)
       res.send(result);
     });
     // user information post dataBD exit
+
+    // user information get start
+    app.get('/users', verifyJwt, verifyAdmin, async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    // user information get end
+
+    // user admin check start
+    app.get('/users/admin/:email', verifyJwt, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false })
+      }
+
+      // jwt verifyJwt start
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+           return res.status(403).send({ error: true, message: 'forbidden access' })
+      }
+      // jwt verifyJwt end
+
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
+    })
+    // user admin check end
+
+    // user admin role added start
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+    // user admin role added exit
+
 
 
     // Send a ping to confirm a successful connection
