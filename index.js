45d9ebcd6bc014bfe9f35e7d6bf4aa096406e3e9
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-08-03 19:42:29 +0600

    paymet part added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+// payment 
+const SSLCommerzPayment = require('sslcommerz-lts')
 // // jwt
 const jwt = require('jsonwebtoken');
 const app = express();
@@ -23,6 +25,10 @@ const client = new MongoClient(uri, {
   }
 });
 
+// payment 
+const store_id = process.env.Store_ID;
+const store_passwd = process.env.Store_PASS;
+const is_live = false //true for live, false for sandbox
 
 // jwt verify start 
 const verifyJwt = (req, res, next) => {
@@ -50,6 +56,7 @@ async function run() {
     const serverCollection = client.db('dbAssignment12').collection('cltAssignment12');
     const selectedCollection = client.db('dbAssignment12').collection('selected');
     const usersCollection = client.db('dbAssignment12').collection('users');
+    const ordersCollection = client.db('dbAssignment12').collection('orders');
     // server link end 
 
     // jwt localhost start
@@ -119,7 +126,6 @@ async function run() {
     })
     //  class data patch end
 
-
     // selected data added post mongoDB start
     app.post('/selected', async (req, res) => {
       const newAdd = req.body;
@@ -148,6 +154,92 @@ async function run() {
     })
     // selected data delete mongoDB  exit
 
+    // class order post mongoDB start
+    const tran_id = new ObjectId().toString();
+
+    app.post('/order', async (req, res) => {
+      const order = req.body
+      const product = await selectedCollection.findOne({
+        _id: new ObjectId(order.productId)
+      })
+
+      const data = {
+        total_amount: product?.price,
+        currency: order?.category,
+        tran_id: tran_id, // use unique tran_id for each api call
+        success_url: `https://assignment12-server-site.vercel.app/payment/success/${tran_id}`,
+        fail_url: 'http://localhost:3030/fail',
+        cancel_url: 'http://localhost:3030/cancel',
+        ipn_url: 'http://localhost:3030/ipn',
+        shipping_method: 'Courier',
+        product_name: product?.name,
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: order?.name,
+        cus_email: order?.email,
+        cus_add1: order?.address,
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: order?.number,
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+      };
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+      sslcz.init(data).then(apiResponse => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL
+        res.send({ url: GatewayPageURL })
+
+        const finalOrder = {
+          product,
+          paidStatus: false,
+          tranjectionId: tran_id,
+        };
+        const result =  ordersCollection.insertOne(finalOrder)
+
+        console.log('Redirecting to: ', GatewayPageURL)
+      });
+
+      app.post('/payment/success/:tranId', async (req, res) => {
+        console.log(req.params.tranId);
+        const result = await ordersCollection.updateOne(
+          {tranjectionId: req.params.tranId},
+          {
+            $set: {
+              paidStatus: true,
+            },
+          }
+        );
+        if (result.modifiedCount > 0) {
+          res.redirect(
+            `http://localhost:5173/dashboard/enrolledClass/${req.params.tranId}`
+          )
+        }
+      })
+      app.post('/payment/fail/:tranId', async (req, res) => {
+        console.log(req.params.tranId);
+        const result = await ordersCollection.deleteOne({
+            tranjectionId: req.params.tranId,
+          });
+        if (result.modifiedCount) {
+          res.redirect(
+            `http://localhost:5173/dashboard/fail/${req.params.tranId}`
+          )
+        }
+      })
+
+    });
+
+    // class order post mongoDB end
 
     // user data post dataBD start 
     app.post('/users', async (req, res) => {
@@ -267,7 +359,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Assignment12 server running')
+  res.send('Music-School server running')
 })
 
 app.listen(port, () => {
